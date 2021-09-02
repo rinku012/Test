a8e7ca2b73b7f6a076472c32f03d695221950434
rinku12 <rinkuyadav03972@gamil.com>
2021-09-02 16:40:30 +0530

    ok git

---
+++ core.java/src/core/java/Marksheet.java
@@ -0,0 +1,10 @@
+package core.java;
+
+public class Marksheet {
+	 
+	public static void main(String[]args) {
+	
+	Marksheet m1=new Marksheet();
+    
+	}
+}
---
+++ core.java/src/core/java/Testsort.java
@@ -0,0 +1,12 @@
+package core.java;
+
+public class Testsort {
+
+	public static void main(String[] args) {
+		
+		Marksheet m1= new Marksheet();
+		
+
+	}
+
+}
